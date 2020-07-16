86cb75a2455920d636872ad2ffa8c804fd13e446
Jessica Leite <jessica.mleite@gmail.com>
2020-07-16 18:25:59 +0100

    Api/submission/higherScore

--- Source/Controllers/SubmissionController.cs
+++ Source/Controllers/SubmissionController.cs
@@ -13,5 +13,27 @@ namespace Codenation.Challenge.Controllers
     [ApiController]
     public class SubmissionController : ControllerBase
     {
+        private readonly ISubmissionService _service;
+        private readonly IMapper _mapper;
+
+        public SubmissionController(ISubmissionService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        // GET api/submission/higherScore
+        [HttpGet("higherScore")]
+        public ActionResult<decimal> GetAll(int? challengeId = null)
+        {
+            if (challengeId.HasValue)
+            {
+                var score = _service.FindHigherScoreByChallengeId(challengeId.Value);
+
+                return Ok(score);
+            }
+
+            return NoContent();
+        }
     }
 }
