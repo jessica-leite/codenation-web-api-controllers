2f41b37d5ca5ccbefd82d436678bd69053ed88df
Jessica Leite <jessica.mleite@gmail.com>
2020-07-16 17:45:11 +0100

    Api/acceleration/id

--- Source/Controllers/AccelerationController.cs
+++ Source/Controllers/AccelerationController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AutoMapper;
+﻿using AutoMapper;
 using Codenation.Challenge.DTOs;
-using Codenation.Challenge.Models;
 using Codenation.Challenge.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +8,23 @@ namespace Codenation.Challenge.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class AccelerationController : ControllerBase
-    {    
+    {
+        private readonly IAccelerationService _service;
+        private readonly IMapper _mapper;
+
+        public AccelerationController(IAccelerationService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        // GET api/acceleration/{id}
+        [HttpGet("{id}")]
+        public ActionResult<AccelerationDTO> Get(int id)
+        {
+            var acceleration = _service.FindById(id);
+
+            return Ok(_mapper.Map<AccelerationDTO>(acceleration));
+        }
     }
 }
