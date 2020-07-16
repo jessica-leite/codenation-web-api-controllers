a8f6ce9f36e35eca058512d9d15b3961524890fa
Jessica Leite <jessica.mleite@gmail.com>
2020-07-16 18:07:52 +0100

    Api/candidate GetAll

--- Source/Controllers/CandidateController.cs
+++ Source/Controllers/CandidateController.cs
@@ -2,6 +2,7 @@
 using Codenation.Challenge.DTOs;
 using Codenation.Challenge.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Codenation.Challenge.Controllers
 {
@@ -25,6 +26,27 @@ namespace Codenation.Challenge.Controllers
             var candidate = _service.FindById(userId, accelerationId, companyId);
 
             return Ok(_mapper.Map<CandidateDTO>(candidate));
+        }
+
+        // GET api/candidate
+        [HttpGet]
+        public ActionResult<IEnumerable<CandidateDTO>> GetAll(int? accelerationId = null, int? companyId = null)
+        {
+            if (accelerationId.HasValue && companyId == null)
+            {
+                var candidates = _service.FindByAccelerationId(accelerationId.Value);
+
+                return Ok(_mapper.Map<IEnumerable<CandidateDTO>>(candidates));
+            }
+
+            if (companyId.HasValue && accelerationId == null)
+            {
+                var candidates = _service.FindByCompanyId(companyId.Value);
+
+                return Ok(_mapper.Map<IEnumerable<CandidateDTO>>(candidates));
+            }
+
+            return NoContent();
         }
     }
 }
