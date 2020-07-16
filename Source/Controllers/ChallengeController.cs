7c830a8c6961aaf89d677a41bec493134d1b5354
Jessica Leite <jessica.mleite@gmail.com>
2020-07-16 16:27:15 +0100

    Api/challenge GET

--- Source/Controllers/ChallengeController.cs
+++ Source/Controllers/ChallengeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AutoMapper;
+﻿using AutoMapper;
 using Codenation.Challenge.DTOs;
 using Codenation.Challenge.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Codenation.Challenge.Controllers
 {
@@ -12,5 +10,27 @@ namespace Codenation.Challenge.Controllers
     [ApiController]
     public class ChallengeController : ControllerBase
     {
+        private readonly IChallengeService _service;
+        private readonly IMapper _mapper;
+
+        public ChallengeController(IChallengeService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        // GET api/challenge
+        [HttpGet]
+        public ActionResult<IEnumerable<ChallengeDTO>> GetAll(int? accelerationId = null, int? userId = null)
+        {
+            if (accelerationId == null && userId == null)
+            {
+                return NoContent();
+            }
+
+            var challenges = _service.FindByAccelerationIdAndUserId(accelerationId.GetValueOrDefault(), userId.GetValueOrDefault());
+
+            return Ok(_mapper.Map<IEnumerable<ChallengeDTO>>(challenges));
+        }
     }
 }
